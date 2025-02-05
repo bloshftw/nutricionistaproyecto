b1c9dbe90c4d77a03d33ad15b468c6e6ba9f7e9a
Luciano Jesus Rodriguez <luciano_rodriguez86@hotmail.com>
2025-02-05 19:21:06 -0300

    Initial commit

---
+++ src/main/java/com/mycompany/nutricionistaproyecto/Nutricionistaproyecto.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.nutricionistaproyecto;
+
+/**
+ *
+ * @author Lucia
+ */
+public class Nutricionistaproyecto {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
